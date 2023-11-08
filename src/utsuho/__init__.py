12b913f66f74032243b12d9fc77ba7165719ec66
Li Yingping <liyp.dl.cn@gmail.com>
2023-11-08 13:38:17 +0800

    Add katakana and hiragana bi-directional converter import

--- src/utsuho/__init__.py
+++ src/utsuho/__init__.py
@@ -1,5 +1,5 @@
 """ Utsuho is an interconverter between Japanese half-width katakana and full-width katakana.
 """
-from .converters import WidthConverterConfig, FullToHalfConverter, HalfToFullConverter
+from .converters import WidthConverterConfig, FullToHalfConverter, HalfToFullConverter, HiraganaToKatakanaConverter, KatakanaToHiraganaConverter
 
 __version__ = '2.0.0'
